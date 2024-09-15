a80a657c3c7cbc50d1f06fcb37400f1714871a49
MallampatiBhavishyaChowdary <146431037+MallampatiBhavishyaChowdary@users.noreply.github.com>
2024-09-15 10:14:31 +0530

    Update app.py

--- 21BCE7418_ML/app.py
+++ 21BCE7418_ML/app.py
@@ -10,3 +10,7 @@ def health_check():
 if __name__ == "__main__":
     app.run(debug=True) #Implement untill here for setup first.Then, we can commit it and check git status.
   
+#Then implementing this code block.This is for /health endpoint to verify our API status.
+@app.route('/health', methods=['GET'])
+def health_check():
+    return jsonify({"status": "API is active!"}), 200. #Commit
